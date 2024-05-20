09a131d1549b1dcf4038d3c53f7ee063f1298098
mangmuse <mangmangmuse@gmail.com>
2024-05-20 15:40:24 +0900

    timeattack-calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+
 export default function App() {
+  const [totalCount, setTotalCount] = useState(0);
+  const [num, setNum] = useState(0);
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input
+          onChange={(e) => setNum(e.target.value)}
+          value={num}
+          type="number"
+        />{" "}
+        만큼을{" "}
+        <button onClick={() => setTotalCount((prev) => prev + +num)}>
+          더할게요
+        </button>
+        <button onClick={() => setTotalCount((prev) => prev - +num)}>
+          뺄게요
+        </button>
+        <button onClick={() => setTotalCount(0)}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{totalCount}</p>
       </div>
     </div>
   );
